97801d80feaa69dad85e89cfbbd522b952918a66
Joey Molinski <molinski826@msn.com>
2024-09-30 13:58:27 -0400

    first commit

---
+++ TacoLab/Controllers/DrinksController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TacoLab.Models;
+
+namespace TacoLab.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DrinksController : ControllerBase
+    {
+        FastFoodTacoDbContext dbContext = new FastFoodTacoDbContext();
+
+        [HttpGet()]
+
+        public IActionResult GetAllDrinks(string sortByCost)
+        {
+            List<Drink> result = dbContext.Drinks.ToList();
+            if (sortByCost != null)
+            {
+                if (sortByCost.ToLower() == "ascending")
+                {
+                    result = result.OrderBy(x => x.Cost).ToList();
+                }
+                else if (sortByCost.ToLower() == "descending")
+                {
+                    result = result.OrderByDescending(x => x.Cost).ToList();
+                }
+                else
+                {
+                    return BadRequest("Order by Ascending or Descending");
+                }
+            }
+            //if (Cost != null)
+            //{
+            //    result = result.OrderBy(x => x.Cost).ToList();
+            //}
+            return Ok(result);
+           
+        }
+        [HttpGet("{Id}")]
+        public IActionResult GetById(int Id)
+        {
+            Drink result = dbContext.Drinks.FirstOrDefault(i => i.Id == Id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+
+                return Ok(result);
+            }
+
+        }
+        [HttpPost()]
+        public IActionResult AddUser([FromBody] Drink newDrink)
+        {
+            //prevents error with increwmenting
+            newDrink.Id = 0;
+            dbContext.Drinks.Add(newDrink);
+            dbContext.SaveChanges();
+
+            return Created($"/api/User/{newDrink.Id}", newDrink);
+        }
+
+        [HttpDelete("{id}")]
+
+        public IActionResult DeleteUser(int id)
+        {
+            Drink u = dbContext.Drinks.FirstOrDefault(x => x.Id == id);
+            if (u == null)
+            {
+                return NotFound("no matching id");
+            }
+            else
+            {
+                dbContext.Drinks.Remove(u);
+                dbContext.SaveChanges();
+                return NoContent();
+            }
+        }
+    }
+}
---
+++ TacoLab/Controllers/TacosController.cs
@@ -0,0 +1,73 @@
+﻿using System.Net.Sockets;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TacoLab.Models;
+
+namespace TacoLab.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TacosController : ControllerBase
+    {
+        FastFoodTacoDbContext dbContext = new FastFoodTacoDbContext();
+
+        [HttpGet()]
+
+        public IActionResult GetAll(string? Name = null, bool? SoftShell = null)
+        {
+            List<Taco> result = dbContext.Tacos.ToList();
+            if(Name != null)
+            {
+                result = result.Where(x => x.Name.ToLower().Trim() == Name.ToLower().Trim()).ToList();
+            }
+            return Ok(result);
+            if (SoftShell != null)
+            { 
+                result = result.Where(x => x.SoftShell == SoftShell).ToList();       
+            }
+        }
+        [HttpGet("{Id}")]
+        public IActionResult GetById(int Id)
+        {
+            Taco result = dbContext.Tacos.FirstOrDefault(i => i.Id == Id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+
+                return Ok(result);
+            }
+
+        }
+        [HttpPost()]
+        public IActionResult AddUser([FromBody] Taco newTaco)
+        {
+            //prevents error with increwmenting
+            newTaco.Id = 0;
+            dbContext.Tacos.Add(newTaco);
+            dbContext.SaveChanges();
+
+            return Created($"/api/User/{newTaco.Id}", newTaco);
+        }
+
+        [HttpDelete("{id}")]
+
+        public IActionResult DeleteUser(int id)
+        {
+            Taco u = dbContext.Tacos.FirstOrDefault(x => x.Id == id);
+            if (u == null)
+            {
+                return NotFound("no matching id");
+            }
+            else
+            {
+                dbContext.Tacos.Remove(u);
+                dbContext.SaveChanges();
+                return NoContent();
+            }
+        }
+
+    }
+}
